a3520a85adacc561c77d923a972cf5b8573a522d
Gonçalo Tomás <goncalo@goncalotomas.com>
2018-08-10 22:10:20 +0100

    implement cuttlefish config suite

---
+++ test/cuttlefish_configuration_SUITE.erl
@@ -0,0 +1,53 @@
+-module(cuttlefish_configuration_SUITE).
+-include_lib("common_test/include/ct.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
+-export([
+    suite/0,
+    all/0,
+    init_per_suite/1,
+    end_per_suite/1
+]).
+
+-export([
+    verify_cuttlefish_params/1
+]).
+
+-define(PING_FAILURE_OUTPUT, "Node did not respond to ping!").
+
+suite() ->
+    [{timetrap, {minutes, 10}}].
+
+init_per_suite(Config) ->
+    rt_ct_util:start_node('cuttlefish_config@127.0.0.1'),
+    rt_ct_util:setup(),
+
+    CuttlefishConf = [
+        {"ring_size", "8"},
+        {"leveldb.sync_on_write", "on"}
+    ],
+
+    [Node] = rt:deploy_nodes(1, {cuttlefish, CuttlefishConf}),
+    [{node, Node} | Config].
+
+end_per_suite(Config) ->
+    Node = ?config(node, Config),
+    rt:stop(Node),
+    ?assertEqual(ok, rt:wait_until_unpingable(Node)),
+    net_kernel:stop(),
+    ok.
+
+all() ->
+    [
+        verify_cuttlefish_params
+    ].
+
+verify_cuttlefish_params(Config) ->
+    Node = ?config(node, Config),
+    {ok, RingSize} = rt:rpc_get_env(Node, [{riak_core, ring_creation_size}]),
+    ?assertEqual(8, RingSize),
+
+    %% test leveldb sync typo
+    {ok, LevelDBSync} = rt:rpc_get_env(Node, [{eleveldb, sync}]),
+    ?assertEqual(true, LevelDBSync),
+    ok.
